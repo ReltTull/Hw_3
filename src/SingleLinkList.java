67138e3c9a9ffb27a0e352ea08d458837ead536b
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-12-19 18:48:30 +0300

    comments added

--- src/SingleLinkList.java
+++ src/SingleLinkList.java
@@ -8,15 +8,15 @@ public class SingleLinkList {
     }
 
     public void addItem (String value)
-    {
+    { // создает ListItem с входным аргументом
         ListItem newItem = new ListItem();
         newItem.value = value;
         if (isEmpty())
-        {
+        { // если список пуст, новый элемент становится и head, и tail
             head = newItem;
             tail = newItem;
         } else
-        {
+        { // если в списке есть элементы, то новый элемент указывается в ссылке предыдущего tail, и сам становится им
             tail.next = newItem;
             tail = newItem;
         }
@@ -25,16 +25,18 @@ public class SingleLinkList {
     public void reverveList ()
     {
         if (!isEmpty() && head.next != null)
-        {
+        { // проверка, есть ли в списке хотя бы два элемента, присваиваем последнему элементу первый
             tail = head;
-            ListItem currentItem = head.next;
-            head.next = null;
+            ListItem currentItem = head.next; // текущий элемент, проход которого по списку будет реверсировать список,
+            // в него "сохраняем" второй элемент
+            head.next = null; // т.к. head теперь - последний элмент списка.
+
             while (currentItem != null)
             {
-                ListItem next = currentItem.next;
-                currentItem.next = head;
+                ListItem next = currentItem.next; // сохраняем ссылку на следующий элемент...
+                currentItem.next = head; // ...и меняем ссылку
                 head = currentItem;
-                currentItem = next;
+                currentItem = next; // шаг на один элемент
             }
         }
     }
