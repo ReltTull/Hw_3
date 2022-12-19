f8a7d27bdf1772607c61f455cfc7ea7726177ca6
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-12-19 18:33:50 +0300

    upload

--- src/SingleLinkList.java
+++ src/SingleLinkList.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class SingleLinkList {
+public class SingleLinkList {
+    ListItem head;
+    ListItem tail;
+
+    public boolean isEmpty()
+    {
+        return head == null;
+    }
+
+    public void addItem (String value)
+    {
+        ListItem newItem = new ListItem();
+        newItem.value = value;
+        if (isEmpty())
+        {
+            head = newItem;
+            tail = newItem;
+        } else
+        {
+            tail.next = newItem;
+            tail = newItem;
+        }
+    }
+
+    public void reverveList ()
+    {
+        if (!isEmpty() && head.next != null)
+        {
+            tail = head;
+            ListItem currentItem = head.next;
+            head.next = null;
+            while (currentItem != null)
+            {
+                ListItem next = currentItem.next;
+                currentItem.next = head;
+                head = currentItem;
+                currentItem = next;
+            }
+        }
+    }
+
+
+    public void printList(ListItem listItem)
+    {
+        if (listItem != null)
+        {
+            System.out.println(listItem.getValue());
+            printList(listItem.next);
+        }
+    }
 }
